85eabaacfef7bca7497329ac0fdd03b1325fa521
ArthurOnWeb <Arthur.rattanavong@etu.univ-savoie.fr>
2023-12-14 15:33:47 +0100

    [FIX] : Front and Back

--- habit-tracker-Front/src/app/app.component.spec.ts
+++ habit-tracker-Front/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'habit-tracker' title`, () => {
+  it(`should have the 'habit-tracker-Front' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('habit-tracker');
+    expect(app.title).toEqual('habit-tracker-Front');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, habit-tracker');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, habit-tracker-Front');
   });
 });
