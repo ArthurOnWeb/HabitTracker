bfbb0a33c4732ae0a6828e459413e0a020a3a5af
Ugo Tafaro <utafaro@gmail.com>
2023-12-22 17:24:30 +0100

    [features] : base de données

--- habit-tracker-Front/src/app/app.component.ts
+++ habit-tracker-Front/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,LoginComponent],
+  imports: [CommonModule, RouterOutlet,LoginComponent, HttpClientModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- habit-tracker-Front/src/app/login/login.component.ts
+++ habit-tracker-Front/src/app/login/login.component.ts
@@ -1,32 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ApiService } from '../api.service';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  username: string = '';
-  email: string = '';
-  password: string = '';
-
-  checkUsername: string = '';
-  checkPasswordInput: string = '';
+  
+  loginObj: any = {
+    username : '',
+    password: ''
+  };
 
   constructor(private apiService: ApiService) {}
+  
+  // createUser() {
+  //   this.apiService.createUser(this.username, this.email, this.password).subscribe(
+  //     response => console.log(response),
+  //     error => console.error(error)
+  //   );
+  // }
 
-  createUser() {
-    this.apiService.createUser(this.username, this.email, this.password).subscribe(
-      response => console.log(response),
-      error => console.error(error)
-    );
-  }
+  
 
   checkPassword() {
-    this.apiService.checkPassword(this.checkUsername, this.checkPasswordInput).subscribe(
+    this.apiService.checkPassword(this.loginObj.username, this.loginObj.password).subscribe(
       response => console.log(response),
       error => console.error(error)
     );
