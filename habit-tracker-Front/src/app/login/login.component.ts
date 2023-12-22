6b275491a59b6b4b555ab081203e38e03c005200
ArthurOnWeb <Arthur.rattanavong@etu.univ-savoie.fr>
2023-12-22 16:04:34 +0100

    [FIX] : Ajout d'utilisateur personalisé possible

--- habit-tracker-Front/src/app/login/login.component.ts
+++ habit-tracker-Front/src/app/login/login.component.ts
@@ -9,17 +9,24 @@ import { ApiService } from '../api.service';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username: string = '';
+  email: string = '';
+  password: string = '';
+
+  checkUsername: string = '';
+  checkPasswordInput: string = '';
+
   constructor(private apiService: ApiService) {}
 
   createUser() {
-    this.apiService.createUser('JohnDoe', 'john@example.com', 'securePassword').subscribe(
+    this.apiService.createUser(this.username, this.email, this.password).subscribe(
       response => console.log(response),
       error => console.error(error)
     );
   }
 
   checkPassword() {
-    this.apiService.checkPassword('JohnDoe', 'securePassword').subscribe(
+    this.apiService.checkPassword(this.checkUsername, this.checkPasswordInput).subscribe(
       response => console.log(response),
       error => console.error(error)
     );
