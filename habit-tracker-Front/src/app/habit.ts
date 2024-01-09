c7d78162933260b6dcda279968917bb5f8b4a7ab
ArthurOnWeb <Arthur.rattanavong@etu.univ-savoie.fr>
2024-01-09 17:05:35 +0100

    [FEATURE] : modèle d'habitude

---
+++ habit-tracker-Front/src/app/habit.ts
@@ -0,0 +1,24 @@
+export class Habit {
+    id: string;
+    username: string;
+    habitName: string;
+    frequency: string;
+    description: string;
+    history: string[];
+  
+    constructor(
+      id: string, 
+      username: string,
+      habitName: string,
+      frequency: string,
+      description: string,
+    ) {
+      this.id = id;
+      this.username = username;
+      this.habitName = habitName;
+      this.frequency = frequency;
+      this.description = description;
+      this.history=[];
+    }
+  }
+  
