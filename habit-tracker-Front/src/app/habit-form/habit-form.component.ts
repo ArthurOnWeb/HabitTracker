4bdc1cc3d21012530d99da5ed98628165cbf787e
Ugo Tafaro <utafaro@gmail.com>
2024-01-10 09:37:57 +0100

    habit

--- habit-tracker-Front/src/app/habit-form/habit-form.component.ts
+++ habit-tracker-Front/src/app/habit-form/habit-form.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { HabitService } from '../habit.service';
 
 @Component({
   selector: 'app-habit-form',
   standalone: true,
-  imports: [CommonModule, NgFor],
+  imports: [CommonModule, NgFor, FormsModule], 
   templateUrl: './habit-form.component.html',
   styleUrl: './habit-form.component.css'
 })
@@ -12,6 +14,12 @@ export class HabitFormComponent {
   days: string[] = ['M', 'T', 'W', 'T', 'F', 'S', 'S'];
 
   nameHabit! : string;
+  
+  constructor(private habitService : HabitService){
+
+  }
 
   
+
+
 }
