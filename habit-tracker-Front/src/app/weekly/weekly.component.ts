f51c106a2976bf99b1b04c8fcae7f749abc14a9b
ArthurOnWeb <Arthur.rattanavong@etu.univ-savoie.fr>
2024-01-11 02:51:16 +0100

    [FEATURE] : weekly component dynamique

--- habit-tracker-Front/src/app/weekly/weekly.component.ts
+++ habit-tracker-Front/src/app/weekly/weekly.component.ts
@@ -16,7 +16,7 @@ import { Router } from '@angular/router';
 export class WeeklyComponent implements OnInit {
 
   days : string[] = ["Mon", "Tue", "Wes", "Thu", "Fri", "Sat", "Sun"];
-
+  today!: Date; // Define a property to store today's date
   habits!: Habit[]; // Define a property to store the retrieved habits
   username: string | undefined; // Define a property to store the username
 
@@ -25,6 +25,7 @@ export class WeeklyComponent implements OnInit {
   ngOnInit() {
     // Call the getHabit function when the component initializes
     this.getHabit();
+    this.today = new Date('2024-01-11T00:00:00'); // Assurez-vous que le fuseau horaire est correct
     
   }
   
@@ -53,6 +54,17 @@ export class WeeklyComponent implements OnInit {
         return true;
       }
       return false;
+  }
+
+  isDayChecked(habit: Habit, day: string): boolean {
+    const dayIndex = this.days.indexOf(day);
+    const todayIndex = this.today.getDay(); // Renvoie le jour de la semaine (0 pour dimanche, 1 pour lundi, etc.)
+    const diff = (dayIndex + 1) - todayIndex; // +1 car getDay() renvoie 0 pour dimanche
+    const targetDate = new Date(this.today);
+    targetDate.setDate(this.today.getDate() + diff);
+
+    // Vérifier si la date formatée est dans l'historique de l'habitude
+    return habit.history.includes(targetDate);
   }
   
 }
